47d8cd4e6ff9c5add08b130ce2705c3cef7184ec
Ceresoli Andrea <Ceresoli.17142>
2021-10-07 13:43:51 +0200

    starting form input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GEP';
+
+  numberOfShops: number | undefined = undefined;
+  numberOfSuppliers: number | undefined = undefined
+
+  inputForm = new FormGroup({
+    numberOfShops: new FormControl(''),
+    numberOfSuppliers: new FormControl(''),
+  });
+
+  keyPressNumbers(event: any) {
+    var charCode = (event.which) ? event.which : event.keyCode;
+    // Only Numbers 0-9
+    if ((charCode < 48 || charCode > 57)) {
+      event.preventDefault();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  dataSubmit(){
+    console.log( this.inputForm.value );
+    var formOutput = this.inputForm.value ;
+    if ( formOutput.numberOfShops == '' || formOutput.numberOfSuppliers == '' ){
+      this.inputForm.setValue( { numberOfShops:'', numberOfSuppliers: '' } );
+      return;
+    }
+
+  }
+
 }
